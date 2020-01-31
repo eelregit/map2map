ea61bb9b658f1fe9745bb4d11b0ae322135c6443
Yin Li <eelregit@gmail.com>
2020-01-31 11:11:31 -0500

    Add exp and log functions from torch

---
+++ map2map/data/norms/torch.py
@@ -0,0 +1,26 @@
+import torch
+
+
+def exp(x, undo=False):
+    if not undo:
+        torch.exp(x, out=x)
+    else:
+        torch.log(x, out=x)
+
+def log(x, eps=1e-8, undo=False):
+    if not undo:
+        torch.log(x + eps, out=x)
+    else:
+        torch.exp(x, out=x)
+
+def expm1(x, undo=False):
+    if not undo:
+        torch.expm1(x, out=x)
+    else:
+        torch.log1p(x, out=x)
+
+def log1p(x, eps=1e-7, undo=False):
+    if not undo:
+        torch.log1p(x + eps, out=x)
+    else:
+        torch.expm1(x, out=x)
