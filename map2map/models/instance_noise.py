d12464955998d3d86268c6e272459b2f546fa555
Yin Li <eelregit@gmail.com>
2020-04-19 02:13:59 -0400

    Simplify instance noise

--- map2map/models/instance_noise.py
+++ map2map/models/instance_noise.py
@@ -2,16 +2,14 @@ from math import log
 import torch
 
 class InstanceNoise:
-    """Instance noise, with a heuristic annealing schedule
+    """Instance noise, with a heuristic decaying schedule
     """
     def __init__(self, init_std, batches):
         self.init_std = init_std
-        self.anneal = 1
-        self.ln2 = log(2)
+        self._std = init_std
         self.batches = batches
 
     def std(self, adv_loss):
-        self.anneal -= adv_loss / self.ln2 / self.batches
-        std = self.anneal * self.init_std
-        std = std if std > 0 else 0
-        return std
+        self._std -= self.init_std / self.batches
+        self._std = self._std if self._std > 0 else 0
+        return self._std
