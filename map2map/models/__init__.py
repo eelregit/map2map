d6b7a528dfa551d2d7479271c3122d792b4f770b
Yin Li <eelregit@gmail.com>
2020-08-04 20:38:21 -0400

    Remove pyramid model

--- map2map/models/__init__.py
+++ map2map/models/__init__.py
@@ -1,6 +1,5 @@
 from .unet import UNet
 from .vnet import VNet, VNetFat
-from .pyramid import PyramidNet
 from .patchgan import PatchGAN, PatchGAN42
 
 from .narrow import narrow_by, narrow_cast, narrow_like
