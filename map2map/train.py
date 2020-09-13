b7a9adf0c7dfe27e47949fada0cb59d0c58e7563
Yin Li <eelregit@gmail.com>
2020-09-13 16:14:48 -0400

    Add pytorch version reporting

--- map2map/train.py
+++ map2map/train.py
@@ -184,6 +184,7 @@ def gpu_worker(local_rank, node, args):
         logger = SummaryWriter()
 
     if rank == 0:
+        print('pytorch {}'.format(torch.__version__))
         pprint(vars(args))
         sys.stdout.flush()
 
