9ae4072fa8ef33dc3b46251e005dbc5966640b72
Yin Li <eelregit@gmail.com>
2021-03-25 17:05:36 -0400

    Change default number of dataloader workers
    as suggested by pytorch

--- map2map/args.py
+++ map2map/args.py
@@ -81,10 +81,9 @@ def add_common_args(parser):
     # "batches" is kept for now for backward compatibility
     parser.add_argument('--batch-size', '--batches', type=int, required=True,
             help='mini-batch size, per GPU in training or in total in testing')
-    parser.add_argument('--loader-workers', default=-8, type=int,
+    parser.add_argument('--loader-workers', default=8, type=int,
             help='number of subprocesses per data loader. '
-            '0 to disable multiprocessing; '
-            'negative number to multiply by the batch size')
+            '0 to disable multiprocessing')
 
     parser.add_argument('--callback-at', type=lambda s: os.path.abspath(s),
             help='directory of custorm code defining callbacks for models, '
@@ -208,8 +207,7 @@ def int_tuple(s):
 
 
 def set_common_args(args):
-    if args.loader_workers < 0:
-        args.loader_workers *= - args.batch_size
+    pass
 
 
 def set_train_args(args):
