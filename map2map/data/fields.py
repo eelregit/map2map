7cc0942cf1853da2a0dc3e31dffdf17f39dfd736
Yin Li <eelregit@gmail.com>
2021-09-24 15:40:21 -0400

    Add support for data of different byte ordering

--- map2map/data/fields.py
+++ map2map/data/fields.py
@@ -193,9 +193,11 @@ class FieldDataset(Dataset):
              self.crop[argsort_perm_axes] * self.scale_factor,
              self.tgt_pad[argsort_perm_axes])
 
-        style = torch.from_numpy(style).to(torch.float32)
-        in_fields = [torch.from_numpy(f).to(torch.float32) for f in in_fields]
-        tgt_fields = [torch.from_numpy(f).to(torch.float32) for f in tgt_fields]
+        style = torch.from_numpy(style.astype(np.float32))
+        in_fields = [torch.from_numpy(f.astype(np.float32))
+                     for f in in_fields]
+        tgt_fields = [torch.from_numpy(f.astype(np.float32))
+                      for f in tgt_fields]
 
         if self.in_norms is not None:
             for norm, x in zip(self.in_norms, in_fields):
